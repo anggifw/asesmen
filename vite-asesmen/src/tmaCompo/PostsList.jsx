3721e3075b4202e1e6ba3240fec01b707538c3d5
Anggi <anggi.kurenai@gmail.com>
2025-07-21 15:47:28 +0700

    Hello World! via vite - nextJS

---
+++ vite-asesmen/src/tmaCompo/PostsList.jsx
@@ -0,0 +1,14 @@
+import Post from './Post';
+
+function Postslist(){
+    return (
+        <ul>
+        <Post author="Anggi" body="Fitriandana" />
+        <Post author="Liverpool FC" body="You'll Never Walk Alone" />
+        </ul>
+
+    );
+
+}
+
+export default Postslist;
